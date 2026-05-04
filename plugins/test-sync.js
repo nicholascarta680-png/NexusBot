b70116774e52243421f1fe94e9024ce3f8070aaa
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-05-04 09:45:57 +0200

    Delete plugins/test-sync.js

--- plugins/test-sync.js
+++
@@ -1,5 +0,0 @@
-let handler = async (m) => {
-    m.reply('*───「 TEST SYNC 」───*\n\n✅ Il sistema di sincronizzazione funziona!\nVersione: `67.0`\n\n*────────────────*')
-}
-handler.command = ['checksync']
-export default handler
