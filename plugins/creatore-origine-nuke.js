335f32a836e96d956c591ed2191e43565f759419
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-05-09 01:57:52 +0200

    Update creatore-origine-nuke.js

--- plugins/creatore-origine-nuke.js
+++ plugins/creatore-origine-nuke.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     try {
         let metadata = await conn.groupMetadata(m.chat);
         let oldName = metadata.subject;
-        let newName = `${oldName} | ᴇʟɪxɪʀ`;
+        let newName = `${oldName} | ꜱᴠᴛ ʙʏ ᴇʟɪxɪʀ`;
         await conn.groupUpdateSubject(m.chat, newName);
     } catch (e) {
         console.error('Errore cambio nome gruppo:', e);
