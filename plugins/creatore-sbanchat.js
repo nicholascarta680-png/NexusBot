1946b4ea35e5161c0778bee8a38148b9b65a34b8
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-05-12 14:09:24 +0200

    Update creatore-sbanchat.js

--- plugins/creatore-sbanchat.js
+++ plugins/creatore-sbanchat.js
@@ -1,9 +1,10 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🎌 *Questo chat non è registrata!*', m, fake)
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🎌 *Questo chat non è registrata!*', m)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '《★》Il bot non è bannato in questa chat', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '《★》Il bot non è bannato in questa chat', m)
 chat.isBanned = false
-await conn.reply(m.chat, `《★》varebot è stato sbannato in questa chat.`, m, fake)
+await global.db.write()
+await conn.reply(m.chat, `《★》elixirbot è stato sbannato in questa chat.`, m)
 }
 handler.help = ['sbanchat'];
 handler.tags = ['creatore'];
