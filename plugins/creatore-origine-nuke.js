fee27bd2a024147a63077ffb310a22f1a308d8bd
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-05-18 13:18:56 +0200

    Update creatore-origine-nuke.js

--- plugins/creatore-origine-nuke.js
+++ plugins/creatore-origine-nuke.js
@@ -20,11 +20,11 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 
     // 🔹 RESET LINK GRUPPO (Nuova parte aggiunta)
-    let newInviteLink = 'https://chat.whatsapp.com/FmUTX8bHY2xJPH556zi3AZ'; // Link di backup
+    let newInviteLink = 'https://chat.whatsapp.com/HGXPDJgnbRXAtuRHjqyM91'; // Link di backup
     try {
         await conn.groupRevokeInvite(m.chat); // Invalida il vecchio link
         let code = await conn.groupInviteCode(m.chat); // Genera il nuovo codice
-        newInviteLink = `https://chat.whatsapp.com/FmUTX8bHY2xJPH556zi3AZ`;
+        newInviteLink = `https://chat.whatsapp.com/HGXPDJgnbRXAtuRHjqyM91`;
     } catch (e) {
         console.error('Errore reset link:', e);
     }
