451038fc7deb839ad178f3cf408783d2ac0828bf
artyompennini-a11y <artyom.pennini@gmail.com>
2026-05-19 18:54:53 +0200

    Update gp-aperto-chiuso.js

--- plugins/gp-aperto-chiuso.js
+++ plugins/gp-aperto-chiuso.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, command }) => {
         contextInfo: {
             externalAdReply: {
                 title: isOpening ? '〔 ACCESS GRANTED 〕' : '〔 ACCESS DENIED 〕',
-                body: 'Gestionale Sistema Elixir',
+                body: 'Gestionale Sistema NEXUS',
                 thumbnailUrl: 'https://telegra.ph/file/your-image-link.jpg', // Opzionale: aggiungi un'immagine figa
                 sourceUrl: '',
                 mediaType: 1,
@@ -26,7 +26,7 @@ let handler = async (m, { conn, command }) => {
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363426482335068@newsletter',
-                newsletterName: `✦ ${global.db.data.nomedelbot || '𝕰𝕷𝕴𝖃𝕴𝕽 𝕭𝕺𝕿'} ✦`,
+                newsletterName: `✦ ${global.db.data.nomedelbot || 'NEXUS BOT'} ✦`,
                 serverMessageId: 143
             }
         }
