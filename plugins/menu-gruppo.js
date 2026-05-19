aeb3e55dfc34d6971f80e711f97f65372c706565
artyompennini-a11y <artyom.pennini@gmail.com>
2026-05-19 19:16:32 +0200

    Rename group from 'elixir' to 'Nexus'

--- plugins/menu-gruppo.js
+++ plugins/menu-gruppo.js
@@ -10,7 +10,7 @@ const localImg = join(process.cwd(), 'menu-gruppo.jpeg');
 const defaultMenu = {
   before: `
 ┎━━━━━━━━━━━━━━━━━━━┑
-┃ ✧ ᴇʟɪxɪʀ - ɢʀᴏᴜᴘ ✧ ┃
+┃ ✧ NEXUS - GROUP ✧ ┃
 ┖━━━━━━━━━━━━━━━━━━━┙
 ┌───────────────────┐
   👤 𝚄𝚜𝚎𝚛: %name
@@ -84,7 +84,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
         mentionedJid: [m.sender],
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363232743845068@newsletter',
-          newsletterName: "✧ ᴇʟɪxɪʀʙᴏᴛ ɢʀᴏᴜᴘ ɢʀᴜᴘᴘᴏ ✧"
+          newsletterName: "✧ NEXUS BOT GROUP GRUPPO ✧"
         }
       }
     }, { quoted: m })
